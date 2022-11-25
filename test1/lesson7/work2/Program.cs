d877e80405135366020b50a840cd2d11c05b3c98
cerber81 <vanya.xarlana@mail.ru>
2022-11-25 22:03:23 +0300

    вск готово 4

---
+++ test1/lesson7/work2/Program.cs
@@ -0,0 +1,53 @@
+﻿int Prompt(string message) // ввод данных
+{
+    System.Console.Write(message); //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine()); // считать строку с консоли и преобразовать в целое значение
+}
+void newarray(int[,] arr) // вывод массива
+{
+ Random random = new Random();
+ for (int i = 0 ; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0 ; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(10, 99);
+                    Console.Write("{0} ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+}
+void FillArray(int[,] matr) // заполнение массива числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10, 11);//[1; 10)
+        }
+    }
+}
+
+int pos1 = Prompt("Введите столбец:");
+int pos2 = Prompt("Введите строку:");
+
+int n = 5; // размер массива
+int m = 7; // размер массива
+int[,] arr = new int[n, m];
+void position(int pos1, int pos2) // найти число по пози....
+{
+if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+    Console.WriteLine("Элемент не существует");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1 -1, pos2 -1]);
+}
+}
+
+FillArray(arr);
+
+Console.WriteLine("Исходный массив:");
+
+newarray(arr);
+position(pos1, pos2);
---
+++ test1/lesson7/work3/Program.cs
@@ -0,0 +1,93 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateRandomArrayOfInt(int rows, int cols, int min, int max) 
+{
+    int[,] temp = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            temp[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return temp;
+}
+
+void PrintArrayOfInt(int[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write($"{array[i, 0]}\t");
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double CalculateAverageOfArray(int[] array) 
+{
+    double temp = array[0] * 1.0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        temp += array[i] * 1.0;
+    }
+    temp /= array.Length;
+    temp = Math.Round(temp, 2);
+    return temp;
+}
+
+int[] TakeColumnFromArray(int[,] array, int col) 
+{
+    int[] temp = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        temp[i] = array[i, col];
+    }
+    return temp;
+}
+
+double[] CalculateAverageForAllColumns(int[,] array) 
+{
+    double[] temp = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp[j] = CalculateAverageOfArray(TakeColumnFromArray(array, j));
+    }
+    return temp;
+}
+
+void PrintAverageForAllColumns(double[] array) 
+{
+    System.Console.WriteLine("------------------------------");
+    System.Console.Write($"{array[0]}\t");
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+void Execute()
+{
+    System.Console.Clear();
+    System.Console.WriteLine("Данная программа генерирует массив из целых чисел и выводит среднеарифметическое элементов каждого столбца.");
+
+    const int rowsInArray = 4;
+    const int colsInArray = 3;
+    const int minOfRandom = -5;
+    const int maxOfRandom = 5;
+
+
+    int[,] arrayOfInt = CreateRandomArrayOfInt(rowsInArray, colsInArray, minOfRandom, maxOfRandom);
+    PrintArrayOfInt(arrayOfInt);
+    PrintAverageForAllColumns(CalculateAverageForAllColumns(arrayOfInt));
+}
+
+Execute();
