869df0df2ba1d2840d83caf8b6284dbcc085d19c
cerber81 <vanya.xarlana@mail.ru>
2022-11-30 20:38:49 +0300

    вск готово 6

---
+++ test1/lesson9/work1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message) // ввод данных
+{
+    System.Console.Write(message); //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine()); // считать строку с консоли и преобразовать в целое значение
+}
+
+int m = Prompt ("Введите число M : ");
+int n = Prompt ("Введите число N : ");
+
+void PrintNumbers(int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    else if (m % 2 == 1)
+    {
+        PrintNumbers(m + 1, n);
+        return;
+    }
+    System.Console.WriteLine(m);
+    PrintNumbers(m + 2, n);
+
+}
+
+PrintNumbers(m, n);
---
+++ test1/lesson9/work2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Prompt(string message) // ввод данных
+{
+    System.Console.Write(message); //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine()); // считать строку с консоли и преобразовать в целое значение
+}
+
+void PrintNumbers(int m, int n, int sum) //натуральные элементы в промежутке от M до N
+{
+    if (n < m)
+    {
+        System.Console.WriteLine(sum);
+        return;
+    }
+  
+    PrintNumbers(m + 1, n, sum + m);
+
+}
+int m = Prompt ("Введите число M : ");
+int n = Prompt ("Введите число N : ");
+
+int sum = 0;
+
+
+PrintNumbers(m, n, sum);
